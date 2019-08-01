287f3538e613ce462f9d5de04e9fc9947645b297
adenfitrajaya <48549205+adenfitrajaya@users.noreply.github.com>
2019-08-01 16:18:39 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# responsiPWM
+# tugasPWM6
+siap
---
+++ aden_sw.js
@@ -0,0 +1,70 @@
+var	CACHE_NAME	=	'aden-cache';
+/*program di atas di gunakan untuk memberikan nama cache yang kita inginkan pada saat riqust ke service worker*/
+var	urlsToCache	=	[
+		'.',
+		'images/gibran1.jpg',
+		'images/gibran2.jpg',
+		'images/gibran3.jpg',
+		'images/gibran4.jpg',
+		'index.html',
+		'nav.html',
+		'pages/offline.html',
+		'pages/post1.html',
+		'pages/post2.html',
+		'pages/post3.html',
+		'pages/home.html',
+		'css/materialize.css',
+		'css/materialize.min.css'
+		
+];
+self.addEventListener('install',	function(event)	{
+		event.waitUntil(
+				caches.open(CACHE_NAME)
+				.then(function(cache)	{
+						return	cache.addAll(urlsToCache);
+				})
+		);
+});
+
+self.addEventListener('fetch',	function(event)	{
+		event.respondWith(
+				caches.match(event.request)
+				.then(function(response)	{
+						return	response	||	fetchAndCache(event.request);
+				})
+		);
+});
+function	fetchAndCache(url)	{
+		return	fetch(url)
+		.then(function(response)	{
+				//	Check	if	we	received	a	valid	response
+				if	(!response.ok)	{
+						throw	Error(response.statusText);
+				}
+				return	caches.open(CACHE_NAME)
+				.then(function(cache)	{
+						cache.put(url,	response.clone());
+						return	response;
+				});
+		})
+		.catch(function(error)	{
+				console.log('Request	failed:',	error);
+				//	You	could	return	a	custom	offline	404	page	here
+		});
+}
+
+
+self.addEventListener('install',	function(event)	{
+		console.log('Service	worker	installing...');
+		/* pada bagian di atas berfungsi untuk menginstal service worker yang di mana akan tampil di bagian console apabila berhasil terinstal*/
+		//	TODO	3.4:	Skip	waiting
+		self.skipWaiting();
+});
+self.addEventListener('activate',	function(event)	{
+		console.log('Service	worker	activating...');
+		/*pada bagian di gunakan untuk mengaktivkan server worker*/
+});
+self.addEventListener('fetch',	function(event)	{
+		console.log('Fetching:',	event.request.url);
+		/*pada bagian di atas berfungsi untuk mengambil data yang mana akan menampilkan URL yang telah di ambil*/
+});
